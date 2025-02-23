5c4641993c4288c531ce583f1f7b64b422fcc8e9
lemoni-ir <info@lemoni.ir>
2025-02-23 18:06:01 +0330

    change structure

--- scripts/install.php
+++ scripts/install.php
@@ -1,2 +1,35 @@
 <?php
-echo "\n" . __FILE__ . "\n";
+echo "Installing Lemoni Core...\n";
+
+$projectRoot = dirname(__DIR__);
+$templatePath = dirname(__DIR__, 1) . "/template";
+
+
+if (!is_dir($templatePath))
+    die("Error: Template folder not found!\nprojectRoot: $projectRoot\ntemplatePath: $templatePath\n");
+
+
+function copy_folder($src, $dst)
+{
+    $dir = opendir($src);
+    @mkdir($dst, 0777, true);
+
+    while (($file = readdir($dir)) !== false) {
+        if ($file === '.' || $file === '..')
+            continue;
+        $srcFile = "$src/$file";
+        $dstFile = "$dst/$file";
+        if (is_dir($srcFile)) {
+            copy_folder($srcFile, $dstFile);
+        } else {
+            copy($srcFile, $dstFile);
+        }
+    }
+    closedir($dir);
+}
+
+copy_folder($templatePath, $projectRoot);
+
+exec("rm -rf $templatePath");
+
+echo "Installation complete!\n";
---
+++ template/public_hml/index.php
@@ -0,0 +1,2 @@
+<?php
+die('Initialize the framework');
